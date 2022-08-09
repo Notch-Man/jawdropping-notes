9cd4b42c8b1df4292b0baed77f054c3dee01f0f6
Notch-Man <samad.narayan@outlook.com>
2022-08-09 17:20:56 +0530

    tag support

--- src/stores.js
+++ src/stores.js
@@ -5,4 +5,5 @@ export const noteTitle = writable("");
 export const noteDescription = writable("");
 export const noteColor = writable("");
 export const noteDate = writable("");
+export const noteTags = writable([]);
 export const notes = writable(JSON.parse(localStorage.getItem("notes")) || []);
